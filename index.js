3d34d582cf4a8af4e20f5b758767535e30123eea
Daniel Ramos <danielramos@MacBook-Air-de-Daniel.local>
2018-10-23 00:09:13 -0300

    Corrigido bug da api com mesmo id

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ import Feed from './src/components/Feed/Feed';
 import { name as appName } from './app.json';
 import Login from './src/components/Login/Login';
 
-AppRegistry.registerComponent(appName, () => Login);
+AppRegistry.registerComponent(appName, () => Feed);
