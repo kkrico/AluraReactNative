3303c5816da48e01234be32e70577321e3223d28
Daniel Ramos <ramos.danielferreira@outlook.com>
2018-10-26 08:34:40 -0300

    Corrigido login automatico

--- index.js
+++ index.js
@@ -3,13 +3,24 @@ import { Navigation } from 'react-native-navigation';
 import Feed from './src/components/Feed/Feed';
 import { name as appName } from './app.json';
 import Login from './src/components/Login/Login';
+import { AsyncStorage } from 'react-native';
 
 Navigation.registerComponent('Login', () => Login);
 Navigation.registerComponent('Feed', () => Feed);
 
-const screen = {
-  screen: 'Login',
-  title: 'Instalura - Login'
-};
+AsyncStorage.getItem('token')
+  .then(token => {
 
-Navigation.startSingleScreenApp({ screen });
+    if (token) {
+      return {
+        screen: 'Feed',
+        title: 'Instalura',
+      };
+    }
+
+    return {
+      screen: 'Login',
+      title: 'Login',
+    };
+  })
+  .then(screen => Navigation.startSingleScreenApp({ screen }));
